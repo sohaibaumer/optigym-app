50e081fa890ff6ab968574c1a0de8b881b815411
Sohaib Aumer <sohaibaumer@hotmail.com>
2022-12-25 14:36:44 +0500

    install tailwindcss and configure theme

---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "gray-20": "#FCF390",
+        "gray-50": "#ECEFF4",
+        "gray-100": "#D3DCE6",
+        "gray-500": "#4A5568",
+        "primary-100": "#8BD8BD",
+        "primary-300": "#62BBA5",
+        "primary-500": "#182441",
+        "secondary-400": "#F29191",
+        "secondary-500": "#D23F57"
+      },
+      backgroundImage: (theme) => ({
+        "mobile-home": "url('./assets/HomePageGraphic.png')"
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"]
+      },
+      content: {
+        optimumtext: "url('./assets/optimumText.png')",
+      }
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    }
+  },
+  plugins: [],
+};
