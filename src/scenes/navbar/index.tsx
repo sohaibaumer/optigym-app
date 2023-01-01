f7a13fa1a618d02e053f0828e060d00c7fb1dca2
Sohaib Aumer <sohaibaumer@hotmail.com>
2023-01-01 16:10:03 +0500

    create and style home page

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
   isTopOfPage: boolean;
@@ -56,9 +57,9 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button >
+                  <ActionButton setSelectedPage={setSelectedPage}>
                     Become a Member
-                  </button>
+                  </ActionButton>
                 </div>
               </div>
             ) : (
