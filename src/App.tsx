174867f9a812c36b52a093311fad749c154b111a
Sohaib Aumer <sohaibaumer@hotmail.com>
2023-01-02 21:17:05 +0500

    add benefits page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
+
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -28,6 +30,7 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />;
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
