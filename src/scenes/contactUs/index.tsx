5769f3c390d8ed41bcb9bd8dad6c9ecdc6415d01
Sohaib Aumer <sohaibaumer@hotmail.com>
2023-01-07 17:21:12 +0500

    create and style contact us page

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,161 @@
+import { useForm } from "react-hook-form";
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import HText from "@/shared/HText";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300
+  px-5 py-3 placeholder-white`;
+
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pb-32 pt-24">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        {/* HEADER */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>
+            JOIN NOW <span className="text-secondary-500">TO GET IN SHAPE</span>
+          </HText>
+          <p className="my-5">
+            Join our vibrant community and embrace the opportunity to push your
+            boundaries, discover new passions, and achieve well-rounded fitness.
+            With our diverse class offerings, you'll never run out of exciting
+            options to keep your workouts fresh, engaging, and effective.
+          </p>
+        </motion.div>
+
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              action="https://formsubmit.co/d8eadbf9e19c7781754c973396cdbe33"
+              method="POST"
+            >
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="NAME"
+                {...register("name", {
+                  required: true,
+                  maxLength: 100,
+                })}
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required."}
+                  {errors.name.type === "maxLength" &&
+                    "Max length is 100 char."}
+                </p>
+              )}
+
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="EMAIL"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+
+              <textarea
+                className={inputStyles}
+                placeholder="MESSAGE"
+                rows={4}
+                cols={50}
+                {...register("message", {
+                  required: true,
+                  maxLength: 2000,
+                })}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required."}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 char."}
+                </p>
+              )}
+
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 text-white transition duration-500 hover:bg-primary-500"
+              >
+                SUBMIT
+              </button>
+            </form>
+          </motion.div>
+
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+              <img
+                className="w-full rounded-md"
+                alt="contact-us-page-graphic"
+                src={ContactUsPageGraphic}
+              />
+            </div>
+          </motion.div>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default ContactUs;
