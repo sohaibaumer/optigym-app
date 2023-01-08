3cf744c9cb8c47d401f4dd333a2d74be38a2199b
Sohaib Aumer <sohaibaumer@hotmail.com>
2023-01-08 11:28:26 +0500

    create and style footer

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,33 @@
+import Logo from "@/assets/Logo.png";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img alt="logo" src={Logo} />
+          <p className="my-5">
+            Elevate your fitness at OptiGym, where excellence meets results.
+            With cutting-edge facilities, diverse classes, and expert trainers,
+            we're here to guide your journey. Join us and unlock your potential
+            for a healthier, stronger, and more confident you.
+          </p>
+          <p>© Optigym All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">https://www.optimumnutrition.com/</p>
+          <p className="my-5">https://www.technogym.com/</p>
+          <p>https://www.muscletech.com/</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">optigym@email.com</p>
+          <p>(444)555-6666</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
